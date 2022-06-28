eb56d51ca7ef188e7621a2cd53788d59afcb41a9
slava liashenko <you@example.com>
2022-06-28 15:16:03 +0300

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Container from "./components/Container/Container";
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+  background-color: #e7ecf2;
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <Container/>
+    </AppContainer>
   );
 }
 
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    box-shadow: 0px 1px 10px grey;
+    border-radius: 10px;
+    background-color: #fff;
+`;
+
+const Description = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const Image = styled.img`
+    margin-top: 60px;
+    margin-bottom: 5px;
+    border-radius: 50%;
+    border: 1px solid #ececec;
+    width: 150px;
+`;
+
+const Name = styled.p`
+    margin-top: 10px;
+    margin-bottom: 5px;
+    font-size: 24px;
+    font-weight: 600;
+`;
+
+const Tag = styled.p`
+    margin-top: 5px;
+    margin-bottom: 5px;
+    font-size: 18px;
+    font-weight: 400;
+    color: #a4aeb8;
+`;
+
+const Location = styled.p`
+    margin-top: 5px;
+    margin-bottom: 5px;
+    font-size: 18px;
+    font-weight: 400;
+    color: #a4aeb8;
+`;
+
+const Stats = styled.ul`
+    margin-top: 40px;
+    list-style: none;
+    display: flex;
+    justify-content: space-between;
+    padding: 0;
+    border-radius: 0px 0px 10px 10px;
+    background-color: #f3f6f9;
+`;
+
+const StatsItem = styled.li`
+    padding: 30px 10px 30px 10px;
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    text-align: center;
+    border-top: 1px solid #ebeff4;
+    border-right: ${props => props.stats && "1px solid #ebeff4"}
+`
+
+const Label = styled.span`
+    color: #a7b2bc;
+    margin-bottom: 2px;
+`;
+
+const Quantity = styled.span`
+    color: #293c51;
+    font-size: 18px;
+    font-weight: 700;
+`;
+
+
+class Profile extends Component {
+    render() {
+        const { username, tag, location, avatar, stats } = this.props
+        return (
+            <Wrapper>
+                <Description>
+                    <Image
+                        src={avatar}
+                        alt="User avatar"
+                    />
+                    <Name>{username}</Name>
+                    <Tag>@{tag}</Tag>
+                    <Location>{location}</Location>
+                </Description>
+
+                <Stats>
+                    <StatsItem stats={stats.followers}>
+                        <Label>Followers</Label>
+                        <Quantity>{stats.followers}</Quantity>
+                    </StatsItem>
+                    <StatsItem stats={stats.views}>
+                        <Label>Views</Label>
+                        <Quantity>{stats.views}</Quantity>
+                    </StatsItem>
+                    <StatsItem>
+                        <Label>Likes</Label>
+                        <Quantity>{stats.likes}</Quantity>
+                    </StatsItem>
+                </Stats>
+        </Wrapper>
+        );
+    }
+}
+
+export default Profile;
